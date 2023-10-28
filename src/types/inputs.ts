cb844578074d5da4938a08d68d1e7f42a9d3066f
Attila Reterics <reterics.attila@gmail.com>
2023-10-28 19:07:46 +0200

    Added Input Components

---
+++ src/types/inputs.ts
@@ -0,0 +1,27 @@
+import {ChangeEventHandler, ReactEventHandler} from "react";
+
+export interface StyledInputArgs {
+    value?: string | number | readonly string[]
+    onChange?: ChangeEventHandler<HTMLInputElement> | undefined,
+    type?: string,
+    name?: string,
+    label?: string|number,
+    placeholder?: string,
+    pattern?: string,
+    maxLength?: number,
+    min?: string,
+    max?: string,
+    step?: string,
+}
+export interface StyledSelectOption {
+    name: string,
+    value: string
+}
+export interface StyledSelectArgs {
+    value?: string | number | readonly string[]
+    onSelect?: ReactEventHandler<HTMLSelectElement> | undefined,
+    type?: string,
+    name?: string,
+    label?: string|number,
+    options: StyledSelectOption[]
+}
